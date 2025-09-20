826284c657cfc17c8af7329b05d411f18d9cba42
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2025-09-20 18:39:03 +0300

    Update LiveDonations component layout and styling
    - Increased the dimensions of the LiveDonations component for improved visibility.
    - Adjusted spacing in the donations list for better separation between items.
    - Expanded the maximum width for donor name and message fields to accommodate longer text, enhancing readability.

--- apps/web/components/home/LiveDonations.tsx
+++ apps/web/components/home/LiveDonations.tsx
@@ -44,7 +44,7 @@ export function LiveDonations({ donations = [], isLoading }: LiveDonationsProps)
     }
 
     return (
-        <div className="w-[322px] h-[580px] bg-white rounded-2xl shadow-lg overflow-hidden p-6">
+        <div className="w-[400px] h-[620px] bg-white rounded-2xl shadow-lg overflow-hidden p-6">
             {/* Header */}
             <div className="w-full rounded-[11px] p-4 border-[0.4px] border-[#FEFEFE] bg-[#BBAC15] flex items-center justify-center">
                 <h2 className=" font-somar font-bold text-2xl leading-none tracking-tight text-center flex items-center justify-center text-white">
@@ -53,17 +53,17 @@ export function LiveDonations({ donations = [], isLoading }: LiveDonationsProps)
             </div>
 
             {/* Donations List */}
-            <div className="py-4 space-y-3">
+            <div className="py-4 space-y-4">
                 {donations.map((donation) => (
                     <div key={donation._id} className="flex w-full items-center justify-between gap-4">
 
                         {/* Donor Name */}
                         <div className=" text-donation-teal  flex items-start  max-w-[130px] flex-col">
-                            <span className="text-lg max-w-[130px]  leading-none line-clamp-1 tracking-tight truncate font-somar font-semibold text-md  text-right">
+                            <span className="text-lg max-w-[200px]  leading-none line-clamp-1 tracking-tight truncate font-somar font-semibold text-md  text-right">
 
                                 {donation.fullName || 'فاعل خير'}
                             </span>
-                            <span className="text-sm max-w-[130px] mt-1 leading-none line-clamp-1 tracking-tight truncate font-somar font-thin text-md  text-right">
+                            <span className="text-sm max-w-[200px] mt-1 leading-none line-clamp-1 tracking-tight truncate font-somar font-thin text-md  text-right">
                                 {donation.message || ''}
                             </span>
                         </div>
